4f42b57ec7c3911e422f607706409aa3f5c14281
Jessica <jessicakchell@gmail.com>
2022-03-09 13:27:18 +0100

    installed lodash & rendered list orderBy descending of the key id

--- src/scripts/Query.js
+++ src/scripts/Query.js
@@ -1,4 +1,5 @@
 import { gql, useQuery } from '@apollo/client';
+import { orderBy } from 'lodash';
 
 const GET_CHARACTERS = gql`
 	query {
@@ -23,20 +24,25 @@ function CharactersList() {
 
 	if (error) return <div>Oops, something is wrong</div>;
 
-	return (
-		<div className='Results'>
-			{data.characters.results.map((character) => {
-				return (
-					<div key={character.id}>
-						<img src={character.image} />
-						<h2>{character.name}</h2>
-						<h4>{character.species}</h4>
-						<h4>{character.gender}</h4>
-					</div>
-				);
-			})}
-		</div>
+	const formatData = data.characters.results.map((character) => {
+		return (
+			<div key={character.id}>
+				<img src={character.image} />
+				<h2>{character.name}</h2>
+				<h4>{character.species}</h4>
+				<h4>{character.gender}</h4>
+			</div>
+		);
+	});
+
+	const orderedData = orderBy(formatData, ['key'], ['desc']);
+
+	console.log(
+		'the list is now rendered by order of the key id descending',
+		orderedData
 	);
+
+	return <div className='Results'>{orderedData}</div>;
 }
 
 export default CharactersList;
